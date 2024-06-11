523ee1940e150e0069df97b23087d8df01a43a82
pdezuani <pablodezuani@gmail.com>
2024-06-11 11:57:22 -0300

    criação do beck completo de login

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated';
+import { DetailuserController } from './controllers/user/DetailUserController';
 
 const router = Router();
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
-
 router.post('/session', new AuthUserController().handle)
-
+router.get('/me', isAuthenticated,  new DetailuserController().handle )
 export { router }; 
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,53 +1,49 @@
 import prismaClient from '../../prisma'
-import{hash} from 'bcryptjs'
-
+import { hash } from 'bcryptjs'
 
 interface UserRequest{
-    name: string;
-    email: string;
-    password: string;
-  }
-  
-  class CreateUserService{
-    async execute({name, email, password}: UserRequest){
-  
-// verificar se ele enviou um email
-  
-if(!email){
-throw new Error("Email incorreto")
-
+  name: string;
+  email: string;
+  password: string;
 }
 
-//verfiicar se o email ja tem acesso 
+class CreateUserService{
+  async execute({ name, email, password }: UserRequest){
 
-const userAlreadyExists = await prismaClient.user.findFirst({
-    where:{
+    // verificar se ele enviou um email
+    if(!email){
+      throw new Error("Email incorrect")
+    }
+
+    //Verificar se esse email já está cadastrado na plataforma
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
         email: email
+      }
+    })
 
+    if(userAlreadyExists){
+      throw new Error("User already exists")
     }
-})
-if(userAlreadyExists){
-    throw new Error("email ja utilizado")
-}
-const passwordHash = await hash(password,8)
-const user = await prismaClient.user.create({
-data:{
-   name: name,
-   email: email,
-   Password:passwordHash,
 
-},
+    const passwordHash = await hash(password, 8)
 
-select:{
-    id:true,
-    name:true,
-    email:true,
-}
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        Password: passwordHash,
+      },
+      select:{
+        id: true,
+        name: true,       
+        email: true,
+      }
+    })
 
-})
 
-      return user
-    }
+    return user;
   }
-  
-  export { CreateUserService }
+}
+
+export { CreateUserService }
