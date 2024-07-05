2ad1d9709881e3a4b5726de1582709e1d3daeb96
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-05 10:05:51 -0300

    melçhorias

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,5 @@ const router = Router();
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
-router.post('/sessions', new AuthUserController().handle)
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
 export { router }; 
