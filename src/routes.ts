84ad10d217eaa4b17b93b99f8a17a03a4f589516
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-05 12:26:04 -0300

    att

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ import { DetailuserController } from './controllers/user/DetailUserController';
 
 const router = Router();
 
-//-- ROTAS USER --
+//-- ROTAS USER --att
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
