c52319d49f1d5df25ee38a5147a57de794973f39
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-05 09:39:24 -0300

    routes add

--- src/routes.ts
+++ src/routes.ts
@@ -10,5 +10,6 @@ const router = Router();
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.post('/sessions', new AuthUserController().handle)
 router.get('/me', isAuthenticated,  new DetailuserController().handle )
 export { router }; 
