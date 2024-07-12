d3dbff16749b6855fd625d34ae7e38d7b19c1ce5
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-12 11:43:31 -0300

    adddatadab

---
+++ prisma/migrations/20240712144320_add_database/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - You are about to drop the `categories` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `items` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `orders` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `products` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "items" DROP CONSTRAINT "items_order_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "items" DROP CONSTRAINT "items_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "products" DROP CONSTRAINT "products_category_id_fkey";
+
+-- DropTable
+DROP TABLE "categories";
+
+-- DropTable
+DROP TABLE "items";
+
+-- DropTable
+DROP TABLE "orders";
+
+-- DropTable
+DROP TABLE "products";
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "valor" DOUBLE PRECISION NOT NULL,
+    "data_da_movimentacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "goals" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "target_amount" DOUBLE PRECISION NOT NULL,
+    "saved_amount" DOUBLE PRECISION NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "goals_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "goals" ADD CONSTRAINT "goals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
