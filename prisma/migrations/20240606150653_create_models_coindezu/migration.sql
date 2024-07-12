96bae4b8c45d3cdc63dde0851f27473e6b116af2
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-12 09:07:02 -0300

    correção

--- prisma/migrations/20240606150653_create_models_coindezu/migration.sql
+++ prisma/migrations/20240606150653_create_models_coindezu/migration.sql
@@ -5,5 +5,7 @@ CREATE TABLE "Movimentacao" (
     "valor" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Movimentacao_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "Movimentacao_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
