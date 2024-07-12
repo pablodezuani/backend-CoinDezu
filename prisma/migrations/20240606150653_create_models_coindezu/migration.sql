2f6afb1247ddd96c53624a0a6b88797217cc1f3c
Pablo Teixeira Dezuani <pdezuani@universal.org>
2024-07-12 09:36:05 -0300

    ......

--- prisma/migrations/20240606150653_create_models_coindezu/migration.sql
+++ prisma/migrations/20240606150653_create_models_coindezu/migration.sql
@@ -47,19 +47,6 @@ CREATE TABLE "items" (
     CONSTRAINT "items_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "transactions" (
-    "id" TEXT NOT NULL,
-    "tipo" TEXT NOT NULL,
-    "valor" FLOAT NOT NULL,
-    "data_da_movimentacao" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
-    "user_id" TEXT NOT NULL,
-
-    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
-);
-
 -- AddForeignKey
 ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -68,6 +55,3 @@ ALTER TABLE "items" ADD CONSTRAINT "items_order_id_fkey" FOREIGN KEY ("order_id"
 
 -- AddForeignKey
 ALTER TABLE "items" ADD CONSTRAINT "items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
